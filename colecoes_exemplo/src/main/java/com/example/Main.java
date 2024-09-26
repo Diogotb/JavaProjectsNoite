b57a1733fda4aee556bec60543d2103997ecd551
Diogotb <digogo@gmail.com>
2024-09-25 21:59:51 -0300

    25.09

---
+++ colecoes_exemplo/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        MapExemplo map = new MapExemplo();
+        map.adicionarNomeIdade("Maria",23);
+        map.adicionarNomeIdade("João",32);
+        map.adicionarNomeIdade("Pedro",55);
+        map.listarNomesIdades();
+        
+
+
+    }
+}
