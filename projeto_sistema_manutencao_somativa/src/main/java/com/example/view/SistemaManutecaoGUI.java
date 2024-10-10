a231ff1e79c6762f3f5b11741869b628a0f1d7d9
Diogotb <digogo@gmail.com>
2024-10-09 21:39:51 -0300

    09.10

--- projeto_sistema_manutencao_somativa/src/main/java/com/example/view/SistemaManutecaoGUI.java
+++ projeto_sistema_manutencao_somativa/src/main/java/com/example/view/SistemaManutecaoGUI.java
@@ -5,38 +5,35 @@ import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 
-public class SistemaManutecaoGUI extends JFrame{
+public class SistemaManutecaoGUI extends JFrame {
     private JTabbedPane tabbedPane;
     private JPanel painelMaquinas;
     private JPanel painelManutencao;
     private JPanel painelFalhas;
     private JPanel painelTecnicos;
-    
 
     public SistemaManutecaoGUI() {
-        //consfigurações iniciais do Frame
+        // consfigurações iniciais do Frame
         super("Sistema de Manutenção");
-        this.setSize(800,600);
+        this.setSize(800, 600);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setLayout(new BorderLayout());
 
-        //inicialição dos paineis
+        // inicialição dos paineis
         painelMaquinas = new MaquinasPanel();
         painelManutencao = new ManutencaoPanel();
         painelFalhas = new FalhasPanel();
         painelTecnicos = new TecnicosPanel();
 
-        //criar meu TabbedPane
+        // criar meu TabbedPane
         tabbedPane = new JTabbedPane();
         tabbedPane.add("Maquinas", painelMaquinas);
         tabbedPane.add("Manutenções", painelManutencao);
         tabbedPane.add("Falhas", painelFalhas);
         tabbedPane.add("Tecnicos", painelTecnicos);
 
+        this.add(tabbedPane, BorderLayout.CENTER);
+
     }
-
-
-
-    
 }
